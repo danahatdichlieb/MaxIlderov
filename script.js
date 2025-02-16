a57fff866bc8001a700b4c465c3a7f44ce8e1aac
Max Ilderov <xdanaatv@gmail.com>
2025-02-16 04:14:59 +0100

    Initial commit

--- script.js
+++ script.js
@@ -33,3 +33,18 @@ document.addEventListener("DOMContentLoaded", function () {
     createStars(150); 
 });
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
